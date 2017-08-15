fc5aa1cb87b42079f1465b5cbe9288ea638d2a61
MartinValchev <martinvalchev1@gmail.com>
2017-08-15 11:29:21 +0300

    EmployeeDaoDBImpl.java implemented

--- src/main/java/com/ibm/cit/employeeStatsWeb/EmployeeStatsWeb/DAO/EmployeeDaoDBImpl.java
+++ src/main/java/com/ibm/cit/employeeStatsWeb/EmployeeStatsWeb/DAO/EmployeeDaoDBImpl.java
@@ -1,21 +1,29 @@
 package com.ibm.cit.employeeStatsWeb.EmployeeStatsWeb.DAO;
 
+import java.sql.Connection;
 import java.util.List;
 
 import com.ibm.cit.employeeStatsWeb.EmployeeStatsWeb.model.Employee;
 
-public class EmployeeDaoDBImpl implements EmployeeDao{
-	
+public class EmployeeDaoDBImpl implements EmployeeDao {
+
 	@Override
 	public Employee getEmployee(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Employee> employeeList = getEmployeeList();
+		Employee employee = employeeList.get(id);
+		return employee;
 	}
 
 	@Override
 	public List<Employee> getEmployeeList() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Employee> employeeList = null;
+		EmployeeJDBC employeeJDBC = new EmployeeJDBC();
+		Connection connection = employeeJDBC.startConnection();
+		if (connection != null) {
+			employeeList = employeeJDBC.generateEmployeeList(connection);
+			employeeJDBC.closeConnection(connection);
+		}
+		return employeeList;
 	}
 
 }
--- src/main/java/com/ibm/cit/employeeStatsWeb/EmployeeStatsWeb/DAO/EmployeeJDBC.java
+++ src/main/java/com/ibm/cit/employeeStatsWeb/EmployeeStatsWeb/DAO/EmployeeJDBC.java
@@ -5,53 +5,70 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibm.cit.employeeStatsWeb.EmployeeStatsWeb.model.Employee;
 
 public class EmployeeJDBC {
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "org.postgresql.Driver";
 	static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/EmployeeDatabase";
-
 	// Database credentials
 	static final String USER = "postgres";
 	static final String PASS = "Postgre12@";
 
-	public void startConnection() {
+	public Connection startConnection() {
 
 		Connection connection = null;
-		Statement stmt = null;
 		try {
 			Class.forName("org.postgresql.Driver");
 			connection = DriverManager.getConnection(DB_URL, USER, PASS);
-			String sql = "SELECT * from public.\"Employee\"";
-			stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			System.out.println(e.getMessage());
+			return connection;
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? " + "Include in your library path!");
+			e.printStackTrace();
+			return null;
+
+		} finally {
+			closeConnection(connection);
+		}
+		return connection;
+	}
+
+	public List<Employee> generateEmployeeList(Connection connection) {
+		String employeesSQL = "SELECT * from public.\"Employee\"";
+		List<Employee> employees = null;
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(employeesSQL);
+			employees = new ArrayList<>();
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String firstName = rs.getString("first_name");
 				String lastName = rs.getString("last_name");
 				double age = rs.getDouble("age");
 				double lengthOfService = rs.getDouble("length_of_service");
-				System.out.println("ID: " + id);
-				System.out.println("first name: " + firstName);
-				System.out.println("last name: " + lastName);
-				System.out.println("Age: " + age);
-				System.out.println("Length Of Service:" + lengthOfService);
+				Employee currentEmployee = new Employee();
+				currentEmployee.setAge(age);
+				currentEmployee.setEmployeeFirstName(firstName);
+				currentEmployee.setEmployeeLastName(lastName);
+				currentEmployee.setId(id);
+				currentEmployee.setLengthOfService(lengthOfService);
+				employees.add(currentEmployee);
 			}
-
 		} catch (SQLException e) {
-
-			System.out.println("Connection Failed! Check output console");
-			System.out.println(e.getMessage());
-			return;
-		} catch (ClassNotFoundException e) {
-
-			System.out.println("Where is your PostgreSQL JDBC Driver? " + "Include in your library path!");
 			e.printStackTrace();
-			return;
-
 		}
 
-		closeConnection(connection);
+		return employees;
+
 	}
 
 	public void closeConnection(Connection conn) {
